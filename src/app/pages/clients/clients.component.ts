8189540fc59fadb2911a5c40f2f25a010f51b025
kalbukov4265 <artur.kalbukov@werush.co>
2019-06-09 20:48:35 +0300

    update clients

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,7 +1,7 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlatformService } from './../../core/services/universal/platform.service';
 import { HeaderService } from './../../core/shared/layouts/layout-components/header/header.service';
 import { BackgroundService, BackgroundColor } from './../../core/shared/layouts/layout-components/background/background.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PageService, PageType } from '@osd-services/page.service';
 import { SeoService } from '@osd-services/seo.service';
 import { TranslationService } from 'app/core/shared/translation/translation.service';
@@ -41,18 +41,15 @@ export class ClientsComponent implements OnInit, OnDestroy {
       this._seo.updateTags(e.acf);
       this.pageContent = e;
       this.pagination = Array(this.pageContent.acf.reviews.length);
-      this.initTestimonialsSlider();
     })
     this._loadClientList();
   }
 
   initTestimonialsSlider() {
     if (this._platform.isBrowser) {
-      setTimeout(() => {
-        this._testimonialsList = document.querySelectorAll('.js-testimonial');
-        this._testimonialWidth = this._testimonialsList[0].scrollWidth;
-        this._track = document.querySelector('.js-track');
-      }, 1000);
+      this._testimonialsList = document.querySelectorAll('.js-testimonial');
+      this._testimonialWidth = this._testimonialsList[0].scrollWidth;
+      this._track = document.querySelector('.js-track');
     }
   }
 
