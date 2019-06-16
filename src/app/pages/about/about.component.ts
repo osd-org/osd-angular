642db611f6adb7950bc74fa634cb670bbde3b654
AlexSumbit <alexsumbit@gmail.com>
2019-06-16 21:15:59 +0300

    about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'app/core/shared/layouts/layout-components/header/header.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private _header: HeaderService
+  ) { }
 
   ngOnInit() {
+    this._header.setTitle('Про нас');
+  }
+
+  ngOnDestroy() {
+    this._header.setTitle('');
   }
 
 }
