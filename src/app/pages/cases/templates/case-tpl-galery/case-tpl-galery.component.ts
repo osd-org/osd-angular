22664a8199a6bfa7b5a4864cdf721442d772032a
AlexSumbit <alexsumbit@gmail.com>
2019-06-29 13:57:48 +0300

    galery tpl layout fix

--- src/app/pages/cases/templates/case-tpl-galery/case-tpl-galery.component.ts
+++ src/app/pages/cases/templates/case-tpl-galery/case-tpl-galery.component.ts
@@ -44,7 +44,7 @@ export class CaseTplGaleryComponent implements OnInit {
   private _initConfig() {
     this.sliderConfig = new Map();
     this.sliderConfig.set(1400, {
-      speed: 1000,
+      speed: 400,
       spaceAround: 5
     })
   }
