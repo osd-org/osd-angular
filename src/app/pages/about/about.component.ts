6a347a7a35f6737ab4e316075dd8b32658445983
kalbukov4265 <artur.kalbukov@werush.co>
2019-07-08 23:51:34 +0300

    add about mobile

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
+import { BackgroundService, BackgroundColor } from './../../core/shared/layouts/layout-components/background/background.service';
 import { HeaderService } from 'app/core/shared/layouts/layout-components/header/header.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PageService, PageType } from '@osd-services/page.service';
 import { SeoService } from '@osd-services/seo.service';
 import { CountUpOptions } from '@osd-directives/countUp/countUp';
 import { untilDestroyed } from '@osd-rxjs/operators'
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { TranslationService } from 'app/core/shared/translation/translation.service';
 
 @Component({
   selector: 'app-about',
@@ -22,11 +25,19 @@ export class AboutComponent implements OnInit, OnDestroy {
   constructor(
     private _header: HeaderService,
     private _page: PageService,
-    private _seo: SeoService
+    private _seo: SeoService,
+    private _background: BackgroundService,
+    private _device: DeviceDetectorService,
+    private _translate: TranslationService
   ) { }
 
   ngOnInit() {
     this._header.setTitle('Про нас');
+    if (this._device.isMobile()) {
+      this._background.changeColor(BackgroundColor.BLACK);
+    } else {
+      this._background.changeColor(BackgroundColor.DARKBLUE);
+    }
     this._page.contentUpdate$.pipe(
       untilDestroyed(this)
     ).subscribe(() => {
@@ -42,4 +53,7 @@ export class AboutComponent implements OnInit, OnDestroy {
     this._header.setTitle('');
   }
 
+  public get LANG(): string {
+    return this._translate.lang;
+  }
 }
