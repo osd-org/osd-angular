a1e3939098d2f9362c81508366575ebb4271e5b5
AlexSumbit <alexsumbit@gmail.com>
2019-07-13 19:39:32 +0300

    mobile home slider

--- src/app/pages/home/home-slider-mobile/home-slider-mobile-slide/home-slider-mobile-slide.component.ts
+++ src/app/pages/home/home-slider-mobile/home-slider-mobile-slide/home-slider-mobile-slide.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home-slider-mobile-slide',
@@ -11,10 +11,31 @@ export class HomeSliderMobileSlideComponent implements OnInit {
   @Input('linesList') linesList;
   @Input('linkData') linkData;
 
-  constructor() { }
+  constructor(
+    public el: ElementRef
+  ) { }
 
   ngOnInit() {
-    console.log(this.linesList);
+    
+  }
+
+  get link(): string {
+    return this.linkData.url;
+  }
+
+  hide (callback: Function = () => {}) {
+    this.el.nativeElement.style.transition = '1s linear';
+    this.el.nativeElement.style.transform = 'translateX(-100%)';
+    this.el.nativeElement.classList.add('hide');
+
+    setTimeout(() => {
+      this.el.nativeElement.classList.remove('hide');
+      this.el.nativeElement.style.transition = 'none';
+      this.el.nativeElement.style.transform = 'translateX(0)';
+      this.el.nativeElement.style.zIndex = '-1';
+
+      callback();
+    }, 1000);
   }
 
 }
