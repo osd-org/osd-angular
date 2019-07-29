66fbae77f154b1ddc1a89ddeda66ab33fed61e91
kalbukov4265 <artur.kalbukov@werush.co>
2019-07-29 18:22:48 +0300

    update blog item

--- src/app/core/shared/components/blog-text/blog-text.component.ts
+++ src/app/core/shared/components/blog-text/blog-text.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, Renderer2, NgZone } from '@angular/core';
-import * as Flickity  from 'flickity';
+import { Component, OnInit, Input, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { debounceTime, skip } from 'rxjs/operators';
 import { BodyService } from '@osd-services/body.service';
@@ -10,20 +9,14 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   templateUrl: './blog-text.component.html',
   styleUrls: ['./blog-text.component.scss']
 })
-export class BlogTextComponent implements OnInit {
-  private _flickity;
-  public maxRangeVal: any;
-  private _currentRangeVal: any;
-  public listLength = 0;
+export class BlogTextComponent implements OnInit, OnDestroy {
+
   private _html: any = null;
+  public progress;
   private _scrollEventMouse$;
-  private _resizeHendler$;
 
   @Input('html')
   set initHTML(v : any[]) {
-    if (this._html) {
-      this._resetSlider();
-    }
     this._html = v;
   }
 
@@ -31,80 +24,22 @@ export class BlogTextComponent implements OnInit {
     return this._html;
   }
 
-  @ViewChild('scrollWrapper') scrollWrapper: ElementRef;
   @ViewChild('renderText') renderText: ElementRef;
-  @ViewChild('bar') bar: ElementRef;
 
   constructor(
-    private _render: Renderer2,
-    private _ngZone: NgZone,
-    private _body: BodyService,
     public device: DeviceDetectorService
   ) {
 
   }
 
   ngOnInit() {
-    if (!this.device.isMobile()) {
-      setTimeout(() => {
-        this._fillingText(() => {
-          this._initSlider();
-        });
-      }, 1000);
-    }
-  }
-
-  private _fillingText(callback) {
-    this._ngZone.run(() => {
-      const content = this.renderText.nativeElement.querySelectorAll('*');
-      let slideList = this.scrollWrapper.nativeElement.querySelectorAll('.carousel-cell');
-      let currentSlide = slideList[slideList.length - 1];
-      let contentHeight = 0;
-      content.forEach((e: HTMLElement) => {
-        if (currentSlide.offsetHeight > contentHeight + e.offsetHeight + 50) {
-          contentHeight = contentHeight + e.offsetHeight;
-          this._render.appendChild(currentSlide, e);
-        } else {
-          this._append();
-          slideList = this.scrollWrapper.nativeElement.querySelectorAll('.carousel-cell');
-          currentSlide = slideList[slideList.length - 1];
-          contentHeight = 0;
-          contentHeight = contentHeight + e.offsetHeight;
-          this._render.appendChild(currentSlide, e);
-        }
-      });
-      callback('finish');
-      this.listLength = slideList.length;
-      this._render.setProperty(this.renderText.nativeElement, 'innerHTML', this._html);
-    });
-  }
-
-  private _initSlider() {
-      this._flickity = new Flickity( '.carousel', {
-        cellAlign: 'left',
-        contain: true,
-        adaptiveHeight: true,
-        prevNextButtons: false,
-        pageDots: false,
-        freeScroll: true
-      });
-      this._scrollEvent$();
-      this._resizeHendler();
-      this._scrollHendler((e) => {
-        this._currentRangeVal = e;
-        this.bar.nativeElement.width = 100 + '%';
-      });
-      this._currentRangeVal = 0;
-  }
-
-  public get currentRangeVal(): number {
-    return this._currentRangeVal;
+    this._scrollEvent$()
   }
 
   private _scrollEvent$() {
-    this._scrollEventMouse$ = fromEvent(this.scrollWrapper.nativeElement, 'wheel')
+    this._scrollEventMouse$ = fromEvent(this.renderText.nativeElement, 'wheel')
     .pipe(
-      debounceTime(300),
+      debounceTime(100),
     )
     .subscribe(e => {
       this._horisontalScrolling(e)
@@ -112,75 +47,10 @@ export class BlogTextComponent implements OnInit {
   }
 
   private _horisontalScrolling(e) {
-    if (e.deltaY > 0) {
-      this._flickity.next();
-    } else {
-      this._flickity.previous();
-    }
+    this.progress = (this.renderText.nativeElement.scrollTop / (this.renderText.nativeElement.scrollHeight - this.renderText.nativeElement.offsetHeight)) * 100;
   }
 
-  public setRange(e) {
-    this._currentRangeVal = e.target.value;
-    this._flickity.select(this.nearest(e.target.value, this.listLength))
-    this._currentRangeVal = this.nearest(e.target.value, this.listLength);
-  }
-
-  private nearest(value, steps, min = 0, max = 100){
-    const stepSize = max / steps;
-    const zerone = Math.round((value - min) * steps/ (max - min)) /steps;
-    if ( ((zerone * (max - min) + min) / stepSize) - 1 === -1) {
-      return 0;
-    } else if ((zerone * (max - min) + min) / stepSize === steps) {
-      return ((zerone * (max - min) + min) / stepSize) - 1;
-    }
-    else {
-      return (zerone * (max - min) + min) / stepSize;
-    }
-  }
-
-  private _scrollHendler(callback) {
-    this._flickity.on( 'scroll', function( event, progress ) {
-      progress = Math.max( 0, Math.min( 1, event )) * 100;
-      callback(progress);
-    });
-  }
-
-  private _append() {
-    const cell = this._render.createElement('div');
-    this._render.addClass(cell, 'carousel-cell');
-    this._render.setStyle(cell, 'width', '100%');
-    this._render.setStyle(cell, 'height', '100%');
-    this._render.appendChild(this.scrollWrapper.nativeElement, cell)
-  }
-
-  public get dots(): any[] {
-    return Array(this.listLength);
-  }
-
-  private _resetSlider() {
-    if (!this.device.isMobile()) {
-      this._flickity.destroy();
-      this._render.setProperty(this.scrollWrapper.nativeElement, 'innerHTML', '');
-      this._append();
-      this._scrollEventMouse$.unsubscribe();
-      this._resizeHendler$.unsubscribe();
-      this.listLength = 0;
-      this._currentRangeVal = 0;
-      setTimeout(() => {
-        this._fillingText(() => {
-          this._initSlider();
-          this._flickity.reloadCells();
-        });
-      }, 1000);
-    }
-  }
-
-  private _resizeHendler() {
-    this._resizeHendler$ = this._body.height$.pipe(
-      skip(1),
-      debounceTime(300)
-    ).subscribe(e => {
-      this._resetSlider();
-    });
+  ngOnDestroy() {
+    this._scrollEventMouse$.unsubscribe();
   }
 }
