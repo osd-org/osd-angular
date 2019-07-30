9d79c58b5bda32144850cb9bf73d03f8cc342319
kalbukov4265 <artur.kalbukov@werush.co>
2019-07-30 17:21:11 +0300

    remove old depend

--- src/app/core/shared/components/components.module.ts
+++ src/app/core/shared/components/components.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SliderTextComponent } from './slider-text/slider-text.component';
 import { RushSliderModule } from './rush-slider/rush-slider.module';
 import { PipesModule } from '@osd-pipes/pipes.module';
 import { BlogTextComponent } from './blog-text/blog-text.component';
 
 @NgModule({
   declarations: [
-    SliderTextComponent,
     BlogTextComponent,
 ],
   imports: [
@@ -16,7 +14,6 @@ import { BlogTextComponent } from './blog-text/blog-text.component';
     PipesModule,
   ],
   exports: [
-    SliderTextComponent,
     RushSliderModule,
     BlogTextComponent
   ]
