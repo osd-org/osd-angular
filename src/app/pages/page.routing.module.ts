a315234469aac0433e6526c8e25ede9f11d4d0b0
kalbukov4265 <artur.kalbukov@werush.co>
2019-08-01 18:22:58 +0300

    add 404

--- src/app/pages/page.routing.module.ts
+++ src/app/pages/page.routing.module.ts
@@ -4,27 +4,34 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageComponent} from './page.component';
 import { LangGuard } from '../core/shared/guards/lang.guard';
 
+
+const routesWithoutLang: Routes = [
+  {path: '404', loadChildren: './not-found/not-found.module#NotFoundModule'},
+];
+
+
 const routes: Routes = [
-    {
-      path: '',
-      component: PageComponent,
-      canActivate: [LangGuard],
-      canActivateChild: [LangGuard],
-      runGuardsAndResolvers: 'always',
-      children: [
-        { path: '', loadChildren: './home/home.module#HomeModule' },
-        { path: ':lang/blog', loadChildren: './blog/blog.module#BlogModule'},
-        { path: ':lang/services', loadChildren: './services/services.module#ServicesModule'},
-        { path: ':lang/cases', loadChildren: './cases/case.module#CaseModule'},
-        { path: ':lang/team', loadChildren: './team/team.module#TeamModule'},
-        { path: ':lang/about', loadChildren: './about/about.module#AboutModule'},
-        { path: ':lang/contact', loadChildren: './contacts/contacts.module#ContactsModule'},
-        { path: ':lang/clients', loadChildren: './clients/clients.module#ClientsModule'},
-        { path: ':lang', loadChildren: './home/home.module#HomeModule' },
-        { path: '**', redirectTo: '/'},
-      ]
-    },
-  ];
+  ...routesWithoutLang,
+  {
+    path: '',
+    component: PageComponent,
+    canActivate: [LangGuard],
+    canActivateChild: [LangGuard],
+    runGuardsAndResolvers: 'always',
+    children: [
+      { path: '', loadChildren: './home/home.module#HomeModule' },
+      { path: ':lang/blog', loadChildren: './blog/blog.module#BlogModule'},
+      { path: ':lang/services', loadChildren: './services/services.module#ServicesModule'},
+      { path: ':lang/cases', loadChildren: './cases/case.module#CaseModule'},
+      { path: ':lang/team', loadChildren: './team/team.module#TeamModule'},
+      { path: ':lang/about', loadChildren: './about/about.module#AboutModule'},
+      { path: ':lang/contact', loadChildren: './contacts/contacts.module#ContactsModule'},
+      { path: ':lang/clients', loadChildren: './clients/clients.module#ClientsModule'},
+      { path: ':lang', loadChildren: './home/home.module#HomeModule' },
+      { path: '**', redirectTo: '/404'},
+    ]
+  },
+];
 
   @NgModule({
     imports: [
